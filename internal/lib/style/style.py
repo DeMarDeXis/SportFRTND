b4e7191b299371a50d24451633e7d5b917112980
Aleksei Lysyakov <Alexrespmor@gmail.com>
2024-09-16 18:03:41 +0300

    Init origin project

---
+++ internal/lib/style/style.py
@@ -0,0 +1,12 @@
+#Style for project
+
+defaultBgColor = '#02091d'
+secondaryBgColor = '#121222'
+defaultFontColor = '#ffffff'
+secondaryFontColor = '#9b9c9c'
+inputBgColor = '#1a1a2a'
+hoverBgColor = '#0068f7'
+
+#Params for window
+defaultWidthWindow = '1000'
+defaultHeightWindow = '600'
---
+++ router.py
@@ -0,0 +1,19 @@
+import flet as ft
+from flet_route import Routing, path
+from pages.signIn import SignInPage
+from pages.signUp import SignUpPage
+
+class Router:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.app_routes = [
+            path(url='/', clear=True, view=SignInPage().view),
+            path(url='/signup', clear=False, view=SignUpPage().view),
+        ]
+
+        Routing(
+            page=self.page,
+            app_routes = self.app_routes,
+        )
+
+        self.page.go(self.page.route)
